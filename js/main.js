3719723831276f399c1c7b4e881830b0f8068eb2
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-12-24 01:57:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,12 +7,20 @@ function getC(link){
 function showdata(elements){
   let result = ``
   let list = elements["results"]
-  
   for(let i = 0; i<=list.length-1;i++){
-      let element =` 
-      <div class="item" <p style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
+    if (list[i].status == 'Alive'){
+      cor = 'rgb(0, 197, 16);'
+    }
+    if (list[i].status == 'Dead'){
+      cor = 'rgb(221, 0, 0);'
+    }
+    if (list[i].status == 'unknown'){
+      cor = 'whitesmoke;'
+    }
+      let element =`
+      <div class="item"  style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
       <p style = "text-align: center;">${list[i].name}</p>
-      <p style = "text-align: center;">${list[i].status}</p>
+      <p style = "color: ${cor};" style = "text-align: center;">${list[i].status}</p>
       </div>
       `
       result+= element     
