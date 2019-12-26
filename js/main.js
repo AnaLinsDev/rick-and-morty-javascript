0b1c16cace72e98f4ba3de31469f2fc557f08b9a
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-12-25 23:02:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,16 +32,16 @@ function showdata(elements){
   text.addEventListener('change', function(){
     result = ''
     for(let i = 0; i<=11;i++){
-      if (text.value == list[i].name){
-            if (list[i].status == 'Alive'){
-      cor = 'rgb(0, 197, 16);'
-    }
-    if (list[i].status == 'Dead'){
-      cor = 'rgb(221, 0, 0);'
-    }
-    if (list[i].status == 'unknown'){
-      cor = 'whitesmoke;'
-    }
+      if (list[i].name.includes(text.value)){
+        if (list[i].status == 'Alive'){
+          cor = 'rgb(0, 197, 16);'
+        }
+        if (list[i].status == 'Dead'){
+          cor = 'rgb(221, 0, 0);'
+        }
+        if (list[i].status == 'unknown'){
+          cor = 'whitesmoke;'
+        }
         let element =`
         <div class="item"  style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
         <p style = "text-align: center;">${list[i].name}</p>
@@ -53,16 +53,3 @@ function showdata(elements){
    item.innerHTML = result
   })}
 
-
-
-
-getC('https://rickandmortyapi.com/api/character/')
-
-
-let button = document.querySelector('.but')
-let form = document.querySelector('fieldset')
-let nome = document.querySelector('#nome')
-button.addEventListener('click', function(){
-  form.innerHTML = `<br><br><br>
-<h1>Obrigado(a) , ${nome.value}</h1>`
-})
